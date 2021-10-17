4f57e1cf668b58bfe9d6cf57a978a68808f018e5
HoTuHi <hotuhi12@gmail.com>
2021-10-17 10:37:36 +0700

    models-Hi

--- src/Model/Quyen.java
+++ src/Model/Quyen.java
@@ -3,29 +3,4 @@ package Model;
 import java.io.Serializable;
 
 public class Quyen  implements Serializable {
-    int id;
-    int quyen;
-    int qma=10000;
-    public Quyen(){
-        this.id=qma++;
-    }
-    public Quyen(int id,int quyen){
-        this.id=id;
-        this.quyen=quyen;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getQuyen() {
-        return quyen;
-    }
-
-    public void setQuyen(int quyen) {
-        this.quyen = quyen;
-    }
 }

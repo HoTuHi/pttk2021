6cb1ced7b06cd470f88e89a5b96b306821605a7f
Nguyen The Nam <namh.ptit@gmail.com>
2021-10-17 02:53:08 +0000

    Update src/Model/TinhTrang.java

--- src/Model/TinhTrang.java
+++ src/Model/TinhTrang.java
@@ -3,4 +3,29 @@ package Model;
 import java.io.Serializable;
 
 public class TinhTrang  implements Serializable {
+    int id;
+    boolean tinhtrang;
+    int tma=1000;
+    public TinhTrang(){
+        this.id=tma++;
+    }
+    public TinhTrang(int id,boolean tinhtrang){
+        this.id=id;
+        this.boolean=boolean;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isTinhtrang() {
+        return tinhtrang;
+    }
+
+    public void setTinhtrang(boolean tinhtrang) {
+        this.tinhtrang = tinhtrang;
+    }
 }
